f90aac89256a191c85378a2e173e16872c89259a
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2024-11-20 08:51:52 +0100

    Have implemented creation tables for employee and tasks

---
+++ SqlDataAccess/Implementation/SqlEmployeeTasksRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using Microsoft.Data.SqlClient;
+using SqlDataAccess.Queries.Exp1;
+
+namespace SqlDataAccess.Implementation
+{
+    public class SqlEmployeeTasksRepository : SqlRepository, IEmployeeTasksRepository
+    {
+        public SqlEmployeeTasksRepository():base("exp1_db"){ }
+        public SqlEmployeeTasksRepository(string database) : base(database) { }
+
+
+        public override void ExecuteCreationAdditional()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ExecuteCreationTable()
+        {
+            string[] statements = GenerateQueriesFromQuery(Experiment1Sql.Tables);
+
+            using var connection = new SqlConnection(_connectionString);
+            connection.Open();
+            foreach (var statement in statements)
+            {
+                var command= new SqlCommand(statement,connection);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void InsertBulk()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int InsertMany()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertOne()
+        {
+            string[] statements = GenerateQueriesFromQuery(Experiment1Sql.Insert);
+        }
+    }
+}
