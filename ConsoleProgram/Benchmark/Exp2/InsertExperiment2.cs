85412d899ec44bb7cd433401d07cc8fe795ae2f5
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2024-12-11 15:35:10 +0100

    Have implemented InsertBenchmarkDotnet

---
+++ ConsoleProgram/Benchmark/Exp2/InsertExperiment2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using ConsoleProgram.Generator;
+using Core.Models.Exp2;
+using DataAccess;
+using SqlDataAccess.Implementation;
+
+namespace ConsoleProgram.Benchmark.Exp2
+{
+    [SimpleJob(RunStrategy.Monitoring, launchCount: 1,
+     warmupCount: 2, iterationCount: 5)]
+    public sealed class InsertExperiment2
+    {
+        public IDepartmentTeamEmployeeRepository _repository;
+        public GeneratorService generatorService= new();
+        public List<DepartmentModel> departmentList;
+        public List<EmployeeModel2> employeeList;
+
+        const int emp_size=50;
+        const int team_size = 20;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            _repository = new SqlDepartementTeamEmployeeRepository();
+            (departmentList, employeeList) = generatorService.GenerateDepartmentsAndEmployeers(emp_size, team_size);
+        }
+
+        [IterationSetup] public void IterationSetup() => _repository.ExecuteCreationTable();
+
+        [Benchmark] public void InsertDepartmentWithTeams() => _repository.InsertDepartmentWithTeams(departmentList.First());
+        [Benchmark] public void InsertEmployee() => _repository.InsertEmployee(employeeList.First());
+
+        [Benchmark] public void InsertDepartmentsWithTeams() => _repository.InsertDepartmenstWithTeams(departmentList);
+        [Benchmark] public void InsertEmployees() => _repository.InsertEmployees(employeeList);
+
+        [Benchmark] public void InsertBulkDepartmentsWithTeamsBulk() => _repository.InsertBulkDepartmenstWithTeams(departmentList);
+        [Benchmark] public void InsertbulkEmployees() => _repository.InsertEmployees(employeeList);
+    }
+}
---
+++ ConsoleProgram/Benchmark/Exp3/InsertExperiment3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using ConsoleProgram.Generator;
+using Core.Models.Exp3;
+using DataAccess;
+using SqlDataAccess.Implementation;
+
+namespace ConsoleProgram.Benchmark.Exp3
+{
+    [SimpleJob(RunStrategy.Monitoring, launchCount: 1,
+     warmupCount: 2, iterationCount: 5)]
+    public class InsertExperiment3
+    {
+        public IEmployeeRepository repository;
+        public GeneratorService generatorService = new();
+        public List<ManagerModel> managers;
+        public List<SoftwareDeveloperModel> softwareDevelopers;
+
+        const int employeeSize = 5000;
+
+        [GlobalSetup] public void GlobalSetup()
+        {
+            repository = new SqlEmployeeRepository();
+            (managers, softwareDevelopers) = generatorService.GenerateDataManagersAndDevelopers(employeeSize);
+        }
+
+        [Benchmark] public void InsertManager() => repository.InsertManager(managers.First());
+        [Benchmark] public void InsertSoftwareDevs() => repository.InsertSoftwareDeveloper(softwareDevelopers.First());
+
+        [Benchmark] public void InsertManyManagers() => repository.InsertManyManager(managers);
+        [Benchmark] public void InsertManyDevelopers() => repository.InsertManySoftwareDeveloper(softwareDevelopers);
+
+        [Benchmark] public void InsertBulkManagers() => repository.InsertBulkManager(managers);
+        [Benchmark] public void InsertBulkSoftwareDevs() => repository.InsertBulkSoftwareDeveloper(softwareDevelopers);
+    }
+}
